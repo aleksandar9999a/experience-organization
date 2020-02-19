633b976f6a1f5c35dff423e7ed8143aba1d373a6
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-19 06:36:05 +0200

    Init

---
+++ src/components/home/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>I am in home</h1>
+</template>
+
+<script>
+export default {
+    name: "home"
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <md-toolbar class="md-accent">
+    <h3 class="md-title">Experience Organization</h3>
+  </md-toolbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style lang="sass" scoped>
+.md-toolbar + .md-toolbar {margin-top: 16px; }
+</style>
