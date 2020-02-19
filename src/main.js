253a4462c07fba85ec75f585c65cd5811cb8d971
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-19 05:46:34 +0200

    Setup firebase, material

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { firestorePlugin } from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+import { environment } from './environments/environments'
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(firestorePlugin)
+Vue.use(VueMaterial)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+export const db = firebase.initializeApp(environment.firebase).firestore()
