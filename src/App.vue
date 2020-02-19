ea461c7cee1cb53b33ee601e57b97e80a558fea4
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-19 06:36:23 +0200

    Added router-view

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <Navbar />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/navbar/Navbar";
+
 export default {
-  name: 'App',
-  components: {
-    
-  }
-}
+  name: "App",
+  components: {Navbar}
+};
 </script>
 
 <style>
