6368e611cfd2dbf05c8e114c21463655a7591ee7
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-20 05:57:16 +0200

    Init

---
+++ src/components/auth/Sign.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <SignIn></SignIn>
+      <SignUp></SignUp>
+  </div>
+</template>
+
+<script>
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+export default {
+    name: 'Sign',
+    components: {
+        SignIn,
+        SignUp
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/auth/SignIn.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="centered-container">
+    <md-content class="md-elevation-3">
+      <div class="title">
+        <img src="./../../assets/logo.png" />
+        <div class="md-title">Experience Organizations</div>
+        <div class="md-body-1">A beautiful application for easy organization tracking!</div>
+      </div>
+
+      <div class="form">
+        <md-field>
+          <label>E-mail</label>
+          <md-input v-model="login.email" autofocus></md-input>
+        </md-field>
+
+        <md-field md-has-password>
+          <label>Password</label>
+          <md-input v-model="login.password" type="password"></md-input>
+        </md-field>
+      </div>
+
+      <div class="actions md-layout md-alignment-center">
+        <md-button class="md-raised md-primary" @click="auth">Log in</md-button>
+      </div>
+
+      <div class="loading-overlay" v-if="loading">
+        <md-progress-spinner md-mode="indeterminate" :md-stroke="2"></md-progress-spinner>
+      </div>
+    </md-content>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignIn",
+  data() {
+    return {
+      loading: false,
+      login: {
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    auth() {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      }, 5000);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.centered-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 100vh;
+  .title {
+    text-align: center;
+    margin-bottom: 30px;
+    img {
+      margin-bottom: 16px;
+      max-width: 150px;
+    }
+  }
+  .actions {
+    .md-button {
+      margin: 0;
+    }
+  }
+  .form {
+    margin-bottom: 60px;
+  }
+  .md-content {
+    z-index: 1;
+    padding: 5%;
+    width: 35%;
+    height: 55%;
+    min-width: 300px;
+    position: relative;
+  }
+  .loading-overlay {
+    z-index: 10;
+    top: 0;
+    left: 0;
+    right: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.9);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
