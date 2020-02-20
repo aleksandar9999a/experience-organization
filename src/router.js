ab1605a61a67ea551612244e91fb74bff9c108a9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-20 05:57:59 +0200

    Added Sign route

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import Home from './components/home/Home'
+import Sign from './components/auth/Sign'
 
 export const organizationRoutes = {
     routes: [
-        {path: '/', component: Home}
+        { path: '/', component: Home },
+        { path: '/auth', component: Sign }
     ],
     mode: 'history'
 }
