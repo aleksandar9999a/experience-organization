d9f0a752fa91f497bba0e204d894d5ac0e369ddb
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-22 20:22:53 +0200

    Added firebase, export router

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { firestorePlugin } from 'vuefire'
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-
-import { environment } from './environments/environments'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
@@ -20,11 +16,9 @@ Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
-const router = new VueRouter(organizationRoutes)
+export const router = new VueRouter(organizationRoutes);
 
 new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
-
-export const db = firebase.initializeApp(environment.firebase).firestore()
