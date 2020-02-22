5b33a36be12f8c1b96bf161d7b0cb65ac9f2bbce
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-22 20:23:28 +0200

    Added signOut action and change navbar state if user is logged...

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -3,26 +3,46 @@
     <img src="./../../assets/logo.png" class="logo" />
     <router-link class="md-title" to="/">Experience Organizer</router-link>
     <div v-if="menuVisible" class="center">
-      <md-button class="md-primary">My Organizer</md-button>
-      <md-button class="md-primary">Search</md-button>
-      <md-button class="md-primary">My profile</md-button>
+      <md-button class="md-primary" to="/organizer">My Organizer</md-button>
+      <md-button class="md-primary" to="/search">Search</md-button>
+      <md-button class="md-primary" to="/profile">My profile</md-button>
     </div>
     <div v-else class="right">
       <md-button class="md-primary" to="/auth">Sign In / Sign Up</md-button>
     </div>
     <div v-if="menuVisible" class="right">
-      <md-button class="md-primary">Log Out</md-button>
+      <md-button class="md-primary" v-on:click="signOut()">Log Out</md-button>
     </div>
   </md-toolbar>
 </template>
 
 <script>
+import { auth } from './../../firebase'
+import { logOut } from './../../services/auth.service'
+
 export default {
   name: "Navbar",
   data: () => {
     return {
       menuVisible: false
     };
+  },
+  methods: {
+    setMenuVisible(state) {
+      this.menuVisible = state;
+    },
+    signOut(){
+      logOut();
+    }
+  },
+  created: function() {
+    auth.onAuthStateChanged(u => {
+      if (u) {
+        this.setMenuVisible(true);
+      } else{
+        this.setMenuVisible(false);
+      }
+    })
   }
 };
 </script>
