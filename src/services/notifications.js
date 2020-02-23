81223572a1d5bbae4237b2fca7ea181afec3cd63
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 18:04:47 +0200

    Added checkList

--- src/services/notifications.js
+++ src/services/notifications.js
@@ -2,24 +2,31 @@ export let listOfNotifications = [];
 let removeNotificationPerInterval = getInterval();
 
 export function addNotification(message) {
-    listOfNotifications.unshift(message);
+    listOfNotifications.splice(1, 0, message);
     if (listOfNotifications.length === 1) {
         removeNotificationPerInterval = getInterval();
     }
 }
 
-export function removeNotificationByIndex(i){
+export function removeNotificationByIndex(i) {
     listOfNotifications.splice(i, 1);
+    checkList();
 }
 
 function removeLastNotification() {
     if (listOfNotifications.length > 0) {
-        listOfNotifications.pop();
-    } else {
+        listOfNotifications.splice(listOfNotifications.length, 1);
+    }
+
+    checkList();
+}
+
+function checkList() {
+    if (listOfNotifications.length === 0) {
         clearInterval(removeNotificationPerInterval);
     }
 }
 
 function getInterval() {
-    return setInterval(removeLastNotification, 3000);
+    return setInterval(removeLastNotification, 4000);
 }
