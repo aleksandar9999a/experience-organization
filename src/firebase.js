705027c7112b9127dbb8c9e51c21a7b8f522c1c3
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 11:08:36 +0200

    Added ;

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,6 @@ import 'firebase/firestore'
 import "@firebase/auth";
 import { environment } from './environments/environments'
 
-export const db = firebase.initializeApp(environment.firebase).firestore()
-export const auth = firebase.auth()
-export const storage = firebase.storage
+export const db = firebase.initializeApp(environment.firebase).firestore();
+export const auth = firebase.auth();
+export const storage = firebase.storage;
