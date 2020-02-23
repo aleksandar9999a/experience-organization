2734d0248e1c6c5ca46e3fafa5d642623962034a
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 14:28:58 +0200

    Init

---
+++ src/components/auth/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="logo-container">
+    <img src="./../../assets/logo.png" />
+    <h1>Experience Organizer</h1>
+    <h5 class="md-body-1">A beautiful application for easy organization tracking!</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logo"
+};
+</script>
+
+<style lang="scss" scoped>
+.logo-container {
+  display: inline-block;
+  text-align: center;
+}
+
+</style>
