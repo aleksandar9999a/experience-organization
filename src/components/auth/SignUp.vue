cceaf603242af5131357e982df842477b3d9a5c4
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 21:26:30 +0200

    Added validations

--- src/components/auth/SignUp.vue
+++ src/components/auth/SignUp.vue
@@ -5,40 +5,71 @@
         <h2 class="title">SIGN UP</h2>
         <md-field>
           <label>E-mail</label>
-          <md-input v-model="reg.email"></md-input>
+          <md-input v-model="email" v-model.trim="$v.email.$model"></md-input>
         </md-field>
+        <div v-if="$v.email.$dirty" class="error">
+          <span v-if="!$v.email.required">Email is required!</span>
+          <span v-else-if="!$v.email.email">Invalid email!</span>
+        </div>
 
         <md-field md-has-password>
           <label>Password</label>
-          <md-input v-model="reg.password" type="password"></md-input>
+          <md-input v-model="password" type="password" v-model.trim="$v.password.$model"></md-input>
         </md-field>
+        <div v-if="$v.password.$dirty" class="error">
+          <span v-if="!$v.password.required">Email is required!</span>
+          <span
+            v-else-if="!$v.password.minLength || !$v.password.maxLength"
+          >Minimum length is 8 chars, max - 20!</span>
+        </div>
 
         <md-field>
           <label>First Name</label>
-          <md-input v-model="reg.firstName"></md-input>
+          <md-input v-model="firstName" v-model.trim="$v.firstName.$model"></md-input>
         </md-field>
+        <div v-if="$v.firstName.$dirty" class="error">
+          <span v-if="!$v.firstName.required">First name is required!</span>
+          <span
+            v-else-if="!$v.firstName.minLength || !$v.firstName.maxLength"
+          >Minimum length is 4 chars, max - 15!</span>
+        </div>
 
         <md-field>
           <label>Last Name</label>
-          <md-input v-model="reg.lastName"></md-input>
+          <md-input v-model="lastName" v-model.trim="$v.lastName.$model"></md-input>
         </md-field>
+        <div v-if="$v.lastName.$dirty" class="error">
+          <span v-if="!$v.lastName.required">Last name is required!</span>
+          <span
+            v-else-if="!$v.lastName.minLength || !$v.lastName.maxLength"
+          >Minimum length is 4 chars, max - 15!</span>
+        </div>
 
         <md-field>
           <label>Gender</label>
-          <md-select v-model="reg.gender" md-dense>
+          <md-select v-model="gender" md-dense v-model.trim="$v.gender.$model">
             <md-option value="M">M</md-option>
             <md-option value="F">F</md-option>
           </md-select>
         </md-field>
+        <div v-if="$v.gender.$dirty" class="error">
+          <span v-if="!$v.gender.required">Gender is required!</span>
+        </div>
 
         <md-field>
           <label>Age</label>
-          <md-input v-model="reg.age"></md-input>
+          <md-input v-model="age" v-model.trim="$v.age.$model"></md-input>
         </md-field>
+        <div v-if="$v.age.$dirty" class="error">
+          <span
+            class="error"
+            v-if="!$v.age.ageValidatior"
+          >Age is required and must be a number between 18 - 60</span>
+        </div>
       </div>
 
       <div class="actions md-layout md-alignment-center">
-        <md-button class="md-raised md-primary" @click="registered">Registered</md-button>
+        <md-button class="md-raised md-primary" @click="registered" :disabled="this.$v.$invalid">Registered</md-button>
       </div>
       <div class="actions md-layout md-alignment-center">
         <md-button class="md-raised md-accent" @click="log">Log In</md-button>
@@ -49,24 +80,53 @@
 
 <script>
 import { signUp } from "./../../services/auth.service";
+import { validationMixin } from "vuelidate";
+import {
+  required,
+  email,
+  minLength,
+  maxLength
+} from "vuelidate/lib/validators";
+import { addNotification } from "./../../services/notifications";
+
+const ageValidatior = value => value >= 18 && value <= 60;
 
 export default {
   name: "SignUp",
   data() {
     return {
-      reg: {
-        email: "",
-        password: "",
-        firstName: "",
-        lastName: "",
-        gender: "",
-        age: ""
-      }
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      gender: "",
+      age: ""
     };
+  },
+  mixins: [validationMixin],
+  validations: {
+    email: { required, email },
+    password: { required, minLength: minLength(8), maxLength: maxLength(20) },
+    firstName: { required, minLength: minLength(4), maxLength: maxLength(15) },
+    lastName: { required, minLength: minLength(4), maxLength: maxLength(15) },
+    gender: { required },
+    age: { required, ageValidatior }
   },
   methods: {
     async registered() {
-      await signUp(this.reg);
+      if (!this.$v.$invalid) {
+        const data = {
+          email: this.email,
+          password: this.password,
+          firstName: this.firstName,
+          lastName: this.lastName,
+          gender: this.gender,
+          age: this.age
+        };
+        await signUp(data);
+      } else {
+        addNotification('Form is invalid!');
+      }
     },
     log() {
       this.$store.commit("changeRegOrLog");
@@ -84,6 +144,14 @@ export default {
 .center {
   margin-left: auto;
   margin-right: auto;
+}
+
+.error {
+  border: 2px;
+  border-color: red;
+  color: red;
+  margin: 0 0 0 15px;
+  padding-bottom: 15px;
 }
 
 .md-field.md-focused label {
