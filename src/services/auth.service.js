6de3549453ea47f4e310aef04a0b59d91210737f
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 12:48:34 +0200

    Added store, show notifications and loader

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,28 +1,41 @@
 import { auth, db, storage } from "./../firebase";
-import { router } from './../main';
+import { router, store } from './../main';
 
 export async function signIn(email, password) {
-    return await auth.signInWithEmailAndPassword(email, password).then(() => { router.push('/') });
+    store.commit('showLoader');
+    return await auth.signInWithEmailAndPassword(email, password).then(() => {
+        store.commit({ type: 'showNotifications', message: 'Successfull Log In!' });
+        router.push('/');
+    }).finally(() => {
+        store.commit('hideLoader');
+    });
 }
 
 export async function signUp(email, password, firstName, lastName, image, gender, age) {
+    store.commit('showLoader');
     return await auth.createUserWithEmailAndPassword(email, password).then((d) => {
-        updateUserData({ uid: d.user.uid, firstName, lastName, image, gender, age }).then(console.log);
-    })
+        updateUserData({ uid: d.user.uid, firstName, lastName, image, gender, age }).then(() => {
+            store.commit({ type: 'showNotifications', message: 'Successfull Registrations!' });
+            router.push('/');
+        });
+    }).finally(() => {
+        store.commit('hideLoader');
+    });
 }
 
 export async function updateUserData(userdata) {
-    if(typeof userdata.image === 'object') {
+    if (typeof userdata.image === 'object') {
         userdata.image = await uploadImage(userdata.image);
     }
     return await db.doc(`users/${userdata.uid}`).set(userdata);
 }
 
 export async function logOut() {
-    return await auth.signOut().then(() => {router.push('/')});
+    store.commit('showLoader');
+    return await auth.signOut().then(() => { router.push('/'); }).finally(() => { store.commit('hideLoader'); });
 }
 
-async function uploadImage(img){
+async function uploadImage(img) {
     const id = db.createId();
     const storageRef = storage.ref(`profileImages/${id}`);
     return await storageRef.put(img).then(getUrl)
