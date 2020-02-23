8c9a4e6812e3642b8e0d526c04c7576209e5ae14
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 15:48:47 +0200

    Added Vuelidate

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
-
 import { firestorePlugin } from 'vuefire'
-
+import Vuelidate from 'vuelidate'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
 import { router } from './router'
-
 import { store } from './store'
+import App from './App.vue'
 
+Vue.use(Vuelidate)
 Vue.use(firestorePlugin)
 Vue.use(VueMaterial)
 
