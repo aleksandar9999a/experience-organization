3e6b61a087b5269071bb8752d869abeb9c15ce30
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 18:04:40 +0200

    Remove notifications state and functions

--- src/store.js
+++ src/store.js
@@ -5,20 +5,10 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     state: {
-        notifications: false,
-        notificationMessage: '',
         loading: false,
         regOrLog: true
     },
     mutations: {
-        showNotifications(state, payload) {
-            state.notifications = true;
-            state.notificationMessage = payload.message;
-            setInterval(() => {
-                state.notifications = false;
-                state.notificationMessage = '';
-            }, 5000);
-        },
         showLoader(state) {
             state.loading = true;
         },
