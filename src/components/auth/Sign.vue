f744b841b9a5ff138a062ac9b192e1b3b148f43a
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 07:53:35 +0200

    Added logo and background

--- src/components/auth/Sign.vue
+++ src/components/auth/Sign.vue
@@ -1,22 +1,48 @@
 <template>
-  <div>
-      <SignIn></SignIn>
-      <SignUp></SignUp>
+  <div class="auth">
+    <div class="center">
+      <div class="logo-container">
+        <img src="./../../assets/logo.png" class="logo block" />
+        <h1>Experience Organizer</h1>
+        <h5 class="md-body-1">A beautiful application for easy organization tracking!</h5>
+      </div>
+    </div>
+    <SignIn></SignIn>
+    <SignUp></SignUp>
   </div>
 </template>
 
 <script>
-import SignIn from './SignIn';
-import SignUp from './SignUp';
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 
 export default {
-    name: 'Sign',
-    components: {
-        SignIn,
-        SignUp
-    }
-}
+  name: "Sign",
+  components: {
+    SignIn,
+    SignUp
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+.auth {
+  background: linear-gradient(110deg, #f8236aad 35%, #fae842 50%) !important;
+}
+
+.logo-container {
+  padding-top: 4%;
+  display: inline-block;
+  text-align: center;
+}
+
+.center {
+  justify-content: center;
+  display: flex;
+}
+
+.logo {
+  width: auto;
+  max-height: 200px;
+}
 </style>
