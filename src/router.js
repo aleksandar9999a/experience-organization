e8504d2f373a78a71628d2d1d73e2f8cff69dd23
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 14:45:55 +0200

    Added all functions connected with router

--- src/router.js
+++ src/router.js
@@ -1,10 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Home from './components/home/Home'
 import Sign from './components/auth/Sign'
+import { auth } from './firebase'
 
-export const organizationRoutes = {
+Vue.use(VueRouter)
+
+const organizationRoutes = {
     routes: [
         { path: '/', component: Home, meta: { requiresAuth: true } },
         { path: '/auth', component: Sign }
     ],
     mode: 'history'
 }
+
+export const router = new VueRouter(organizationRoutes);
+
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+    if (requiresAuth) {
+        auth.onAuthStateChanged(u => u ? next() : next('/auth'));
+    } else {
+        next();
+    }
+})
