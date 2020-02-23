403b9535f678811bd69cf57ddff20250bb6b04c4
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 14:46:27 +0200

    Clean code and import router, store

--- src/main.js
+++ src/main.js
@@ -2,63 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { firestorePlugin } from 'vuefire'
-import { auth } from './firebase'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
-import VueRouter from 'vue-router'
-import { organizationRoutes } from './router'
+import { router } from './router'
 
-import Vuex from 'vuex'
+import { store } from './store'
 
-Vue.use(Vuex)
 Vue.use(firestorePlugin)
 Vue.use(VueMaterial)
-Vue.use(VueRouter)
 
 Vue.config.productionTip = false
-
-export const router = new VueRouter(organizationRoutes);
-
-export const store = new Vuex.Store({
-  state: {
-    notifications: false,
-    notificationMessage: '',
-    loading: false,
-    regOrLog: true
-  },
-  mutations: {
-    showNotifications(state, payload) {
-      state.notifications = true;
-      state.notificationMessage = payload.message;
-      setInterval(() => {
-        state.notifications = false;
-        state.notificationMessage = '';
-      }, 5000);
-    },
-    showLoader(state) {
-      state.loading = true;
-    },
-    hideLoader(state) {
-      state.loading = false;
-    },
-    changeRegOrLog(state) {
-      state.regOrLog = !state.regOrLog;
-    }
-  }
-})
-
-
-router.beforeEach((to, from, next) => {
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-  if (requiresAuth) {
-    auth.onAuthStateChanged(u => u ? next() : next('/auth'));
-  } else {
-    next();
-  }
-})
 
 new Vue({
   router,
