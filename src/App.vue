9e6787eb3f8dddfefab9c784e92a0e42dda5f85a
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-23 11:09:56 +0200

    Added menuVisible, setMenuVisible, and subscribe for auth

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Navbar />
+    <Navbar v-if="menuVisible" />
     <router-view></router-view>
     <Notifications></Notifications>
   </div>
@@ -9,12 +9,33 @@
 <script>
 import Navbar from "./components/navbar/Navbar";
 import Notifications from "./components/notifications/Notifications";
+import { auth } from './firebase';
+
 
 export default {
   name: "App",
   components: {Navbar, Notifications},
+  data: () => {
+    return {
+      menuVisible: false
+    };
+  },
+  methods: {
+    setMenuVisible(state) {
+      this.menuVisible = state;
+    }
+  },
   errorCaptured: function (err) {
     this.$emit('show:notification', err.message);
+  },
+  created: function() {
+    auth.onAuthStateChanged(u => {
+      if (u) {
+        this.setMenuVisible(true);
+      } else{
+        this.setMenuVisible(false);
+      }
+    })
   }
 };
 </script>
