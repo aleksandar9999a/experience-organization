8c9fd4b1934c5f671db238fbf7959b3d4179990c
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-24 05:47:50 +0200

    Added emty state

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,13 +1,39 @@
 <template>
-    <h1>I am in home</h1>
+  <div class="home">
+    <md-empty-state md-rounded :md-size="size">
+      <img src="./../../assets/logo.png" />
+      <p class="md-headline">Experience Organizer</p>
+      <p class="md-subheading">Create your organization and track it easy!</p>
+      <md-button class="md-primary md-raised">Create</md-button>
+    </md-empty-state>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "home"
-}
+  name: "home",
+  data: function(){
+      return {
+          size: 520
+      }
+  }
+};
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+    .home{
+        min-height: 600px;
+        display: flex;
+    }
+
+    .md-empty-state{
+        margin-bottom: auto;
+        margin-top: auto;
+        max-width: 1000px !important;
+    }
+
+    p{
+        margin-block-start: 0;
+    }
 
 </style>
