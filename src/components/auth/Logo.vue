8ca7d51230a056430e4c6d04c05157793f127779
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-24 06:10:19 +0200

    Change title style

--- src/components/auth/Logo.vue
+++ src/components/auth/Logo.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="logo-container">
     <img src="./../../assets/logo.png" />
-    <h1>Experience Organizer</h1>
-    <h5 class="md-body-1">A beautiful application for easy organization tracking!</h5>
+    <h1 class="md-display-3">Experience Organizer</h1>
+    <h5 class="md-subheading">A beautiful application for easy organization tracking!</h5>
   </div>
 </template>
 
@@ -16,6 +16,10 @@ export default {
 .logo-container {
   display: inline-block;
   text-align: center;
+}
+
+.md-display-3{
+  color: black;
 }
 
 </style>
