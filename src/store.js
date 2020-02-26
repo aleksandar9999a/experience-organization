9b8c66c8bfd933097cf3d32c9eb4fa3f0601af4c
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-26 05:47:05 +0200

    Remove loading

--- src/store.js
+++ src/store.js
@@ -5,16 +5,9 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     state: {
-        loading: false,
         regOrLog: true
     },
     mutations: {
-        showLoader(state) {
-            state.loading = true;
-        },
-        hideLoader(state) {
-            state.loading = false;
-        },
         changeRegOrLog(state) {
             state.regOrLog = !state.regOrLog;
         }
