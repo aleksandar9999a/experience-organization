7c709d6c1f5906e95b16bd989d70f4a2a0547cc4
aleksandar9999a <aleksandar9999a@gmail.com>
2020-02-29 18:31:12 +0200

    Added getStepperState

--- src/components/create-project/CreateProject.vue
+++ src/components/create-project/CreateProject.vue
@@ -1,18 +1,18 @@
 <template>
-    <md-steppers md-alternative>
-      <md-step id="first" md-label="Image">
+    <md-steppers :md-active-step.sync="stepper.active" md-alternative>
+      <md-step id="first" md-label="Image" :md-done.sync="stepper.first">
         <ProjectImage></ProjectImage>
       </md-step>
 
-      <md-step id="second" md-label="Title">
+      <md-step id="second" md-label="Title" :md-done.sync="stepper.second">
         <ProjectName></ProjectName>
       </md-step>
 
-      <md-step id="third" md-label="Team">
+      <md-step id="third" md-label="Team" :md-done.sync="stepper.third">
         <ProjectMembers></ProjectMembers>
       </md-step>
 
-      <md-step id="four" md-label="Steps">
+      <md-step id="four" md-label="Steps" :md-done.sync="stepper.four">
         <ProjectSteps></ProjectSteps>
       </md-step>
     </md-steppers>
@@ -23,10 +23,19 @@ import ProjectImage from "./ProjectImage";
 import ProjectName from "./ProjectName";
 import ProjectMembers from "./ProjectMembers";
 import ProjectSteps from "./ProjectSteps";
+import { getStepperState } from './../../services/create-project.service';
 
 export default {
   name: "CreateProject",
-  components: { ProjectImage, ProjectName, ProjectMembers, ProjectSteps }
+  components: { ProjectImage, ProjectName, ProjectMembers, ProjectSteps },
+  data: function(){
+    return {
+      stepper: {}
+    }
+  },
+  created: function(){
+    return this.stepper = getStepperState();
+  }
 };
 </script>
 
