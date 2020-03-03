c11cdaace51f35a22817b2e9d1313446f0e9c875
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-03 11:11:40 +0200

    Added storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,10 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/storage';
 import "@firebase/auth";
 import { environment } from './environments/environments';
 
 export const db = firebase.initializeApp(environment.firebase);
 export const firestore = db.firestore();
 export const auth = firebase.auth();
-export const storage = firebase.storage;
+export const storage = firebase.storage();
