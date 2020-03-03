527a82f606ef3a7dac3d35b8137b4ee25d34e7d9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-03 09:58:13 +0200

    Added toolbar and menu

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,80 @@
 <template>
-  <div class="app">
-    <Navbar v-if="menuVisible" />
-    <router-view></router-view>
-    <Notifications></Notifications>
-    <Loader></Loader>
+  <div class="page-container">
+    <md-app>
+      <md-app-toolbar class="md-transparent" md-mode="reveal" md-elevation="0" v-if="menuVisible">
+        <md-button class="md-icon-button" @click="changeMenuState">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <md-button class="logo" to="/">
+          <img src="./assets/logo.png" class="logo" /> Experience Organizer
+        </md-button>
+      </md-app-toolbar>
+      <md-app-drawer :md-active.sync="isVisible" v-if="menuVisible">
+        <Menu />
+      </md-app-drawer>
+      <md-app-content>
+        <router-view></router-view>
+        <Notifications></Notifications>
+        <Loader></Loader>
+      </md-app-content>
+    </md-app>
   </div>
 </template>
 
 <script>
-import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
 import Notifications from "./components/notifications/Notifications";
-import Loader from './components/loader/Loader';
-import { auth } from './firebase';
-import { addNotification } from './services/notifications';
+import Loader from "./components/loader/Loader";
+import { auth } from "./firebase";
+import { addNotification } from "./services/notifications";
 
 export default {
   name: "App",
-  components: {Navbar, Notifications, Loader},
+  components: { Menu, Notifications, Loader },
   data: () => {
     return {
-      menuVisible: false
+      menuVisible: false,
+      isVisible: false
     };
   },
   methods: {
     setMenuVisible(state) {
       this.menuVisible = state;
+    },
+    changeMenuState() {
+      this.isVisible = !this.isVisible;
     }
   },
-  errorCaptured: function (err) {
+  errorCaptured: function(err) {
     addNotification(err.message);
   },
   created: function() {
-    auth.onAuthStateChanged(u => {
-      if (u) {
-        this.setMenuVisible(true);
-      } else{
-        this.setMenuVisible(false);
-      }
-    })
+    auth.onAuthStateChanged(u => this.setMenuVisible(!!u));
   }
 };
 </script>
 
 <style>
-.app {
-  background: linear-gradient(to top, #2c3868, #fad7e1);
+.md-app-content{
+   padding: 0;
+   background: linear-gradient(to right, #ffefba, #ffffff) !important;
+}
+
+.md-app {
   min-height: 1000px;
+  border: 1px solid rgba(#000, 0.12);
+  background: linear-gradient(to right, #ffefba, #ffffff) !important;
+}
+
+.md-drawer {
+  width: 400px;
+  max-width: calc(100vw - 125px);
+}
+
+.logo {
+  width: auto;
+  height: 60px !important;
+  margin-left: auto !important;
+  margin-right: auto !important;
 }
 </style>
