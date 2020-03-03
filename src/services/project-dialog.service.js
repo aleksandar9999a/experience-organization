95fddc9631372871502c70bda6d464a150dcc8d9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-03 19:46:19 +0200

    Added more states, getters, setters, checkDataIsValid, submitProject

--- src/services/project-dialog.service.js
+++ src/services/project-dialog.service.js
@@ -1,11 +1,111 @@
 import Vue from 'vue';
+import { addNotification } from './notifications';
+import { createProject } from './firestore.service';
 
-const dialog = Vue.observable({showDialog: false});
+const dialog = Vue.observable({
+    showDialog: false,
+    name: '',
+    description: '',
+    members: [],
+    steps: [{id: '1', name: 'Write name!', description: 'Write description!'}]
+});
 
 export function getShowDialog(){
     return dialog.showDialog;
 }
 
+export function getName(){
+    return dialog.name;
+}
+
+export function setName(newName){
+    return dialog.name = newName;
+}
+
+export function getDesc(){
+    return dialog.description;
+}
+
+export function setDesc(desc){
+    return dialog.description = desc;
+}
+
+export function getMembers(){
+    return dialog.members;
+}
+
+export function getSteps(){
+    return dialog.steps;
+}
+
+export function addEmptyStep(){
+    const id = dialog.steps.length + 1;
+    return dialog.steps.push({
+        id: id.toString(),
+        name: 'Write name!',
+        description: 'Write description'
+    })
+}
+
+export function removeMember(index){
+    return dialog.members.splice(index, 1);
+}
+
 export function changeShowDialog(){
     return dialog.showDialog = !dialog.showDialog;
+}
+
+function checkDataIsValid(){
+    if (dialog.name.length < 4 || dialog.name.length > 30) {
+
+        addNotification('Project name must be between 4 and 30 chars.');
+        return false;
+    }
+    if (dialog.description.length < 4 || dialog.description.length > 300) {
+        addNotification('Project description must be between 4 and 300 chars.');
+        return false;
+    }
+    if (dialog.members.length < 1 || dialog.members.length > 12) {
+        addNotification('Members must be between 1 and 12.');
+        return false;
+    }
+    if (dialog.steps.length < 2 || dialog.steps.length > 20) {
+        addNotification('Steps must be between 2 and 20.');
+        return false;
+    }
+
+    const stepsErrors = dialog.steps.map(checkStep);
+    if (stepsErrors.includes(false)) {
+        return false;
+    }
+
+    return true;
+}
+
+function checkStep(step) {
+    if(step.name.length < 4 || step.name.length > 30){
+        addNotification(`${step.id} is with invalid name! Must be between 4 and 30 chars.`);
+        return false;
+    }
+    if(step.description.length < 4 || step.description.length > 300){
+        addNotification(`${step.id} is with invalid description! Must be between 4 and 300 chars.`);
+        return false;
+    }
+
+    return true;
+}
+
+export function submitProject(){
+    if(checkDataIsValid()){
+        const project = {
+            name: dialog.name,
+            description: dialog.description,
+            members: dialog.members,
+            steps: dialog.steps,
+            status: 'Active'
+        };
+        addNotification('Time to create a new project!');
+        return createProject(project);
+    }
+    return null;
 }
