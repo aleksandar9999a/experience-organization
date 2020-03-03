c9554cfbbdc95ad7106ca93983dc7a182ae314fb
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-03 10:22:12 +0200

    Render profile information

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,8 +1,13 @@
 <template>
-  <md-list>
-    <md-avatar class="md-large">
-      <img src="/assets/examples/avatar-2.jpg" alt="People" />
-    </md-avatar>
+  <md-list class="menu">
+    <md-list-item class="profile" v-if="user">
+      <div class="info">
+        <md-avatar class="md-large">
+          <img :src="user.image" alt="People" />
+        </md-avatar>
+        <p class="md-subheading">{{user.firstName}} {{user.lastName}}</p>
+      </div>
+    </md-list-item>
 
     <md-list-item to="/home">
       <md-icon>dashboard</md-icon>
@@ -19,7 +24,7 @@
       <span class="md-list-item-text">Search</span>
     </md-list-item>
 
-    <md-list-item  @click="signOut">
+    <md-list-item @click="signOut">
       <md-icon>exit_to_app</md-icon>
       <span class="md-list-item-text">Log Out</span>
     </md-list-item>
@@ -28,16 +33,47 @@
 
 <script>
 import { logOut } from "./../../services/auth.service";
+import { getMyData } from "./../../services/firestore.service";
 
 export default {
   name: "Menu",
+  data: function() {
+    return {
+      user: null
+    };
+  },
   methods: {
     signOut() {
       logOut();
     }
+  },
+  created: function() {
+    this.$bind("user", getMyData());
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.menu{
+    height: 100%;
+    background: linear-gradient(to top, #626772, #3f89ff); 
+}
+
+.profile {
+  display: flex;
+  margin-bottom: 50px;
+}
+
+.info{
+    margin: auto;
+}
+
+.md-avatar{
+    width: 100%;
+    height: 120px;
+}
+
+.md-subheading{
+    text-align: center;
+}
 </style>
