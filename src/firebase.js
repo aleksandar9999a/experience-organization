7b40896948b5f0d503204f3a9078924f55f071a9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-03 10:22:38 +0200

    Export firebase and firestore

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,7 @@ import 'firebase/firestore';
 import "@firebase/auth";
 import { environment } from './environments/environments';
 
-export const db = firebase.initializeApp(environment.firebase).firestore();
+export const db = firebase.initializeApp(environment.firebase);
+export const firestore = db.firestore();
 export const auth = firebase.auth();
 export const storage = firebase.storage;
