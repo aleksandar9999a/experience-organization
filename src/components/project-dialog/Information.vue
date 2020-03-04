26364937f04108b0c42c6877354e8609d41d06ab
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-04 05:53:01 +0200

    Added watch, remove handlesChanges

--- src/components/project-dialog/Information.vue
+++ src/components/project-dialog/Information.vue
@@ -2,40 +2,69 @@
   <div>
     <md-field>
       <label>Project Name</label>
-      <md-input v-model="name" @change="handleChanges('name')" required></md-input>
+      <md-input v-model="name" required></md-input>
     </md-field>
     <md-field>
       <label>Description</label>
-      <md-textarea v-model="description" @change="handleChanges('desc')" required></md-textarea>
+      <md-textarea v-model="description" required></md-textarea>
     </md-field>
+    <div class="date">
+      <div class="block">
+        <md-datepicker v-model="start">
+          <label>Start Date</label>
+        </md-datepicker>
+      </div>
+      <div class="block">
+        <md-datepicker v-model="end">
+          <label>End Date</label>
+        </md-datepicker>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { setName, setDesc } from "./../../services/project-dialog.service";
+import {
+  setName,
+  setDesc,
+  setStartDate,
+  setEndDate
+} from "./../../services/project-dialog.service";
 
 export default {
   name: "Information",
-  data: function(){
+  data: function() {
     return {
-      name: '',
-      description: ''
-    }
+      name: "",
+      description: "",
+      start: null,
+      end: null
+    };
   },
-  methods: {
-    handleChanges(type){
-      if (type === 'name') {
-        return setName(this.name);
-      } 
-      if (type === 'desc') {
-        return setDesc(this.description);
-      }
-
-      return null;
+  watch: {
+    start: function() {
+      setStartDate(this.start);
+    },
+    end: function() {
+      setEndDate(this.end);
+    },
+    name: function() {
+      setName(this.name);
+    },
+    description: function() {
+      setDesc(this.description);
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.date {
+  display: flex;
+  justify-content: center;
+}
+
+.block + .block {
+  padding-left: 50px;
+}
 </style>
