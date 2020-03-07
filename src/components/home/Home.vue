4744c57c06769a3f3a2b6f0598a2a943275486a4
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-07 05:49:08 +0200

    Added List component and remove md-list

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -11,30 +11,7 @@
     <div v-else>
       <h1 class="md-display-3">List of Projects</h1>
       <p class="md-subheading">The list contains all projects where you are a member.</p>
-      <md-list class="md-double-line">
-        <div v-bind:style="{height: (72 * documents.length) + 'px'}">
-          <div class="color"></div>
-          <div class="list">
-            <md-list-item v-for="doc of documents" :key="doc.id" :to="'/details/' + doc.id">
-              <div class="project-name">
-                <span class="md-list-item-text class item-title">Project Name</span>
-                <span class="md-list-item-text">{{doc.name}}</span>
-              </div>
-              <div v-if="doc.status === 'Active'">
-                <span class="md-list-item-text item-title">Start Time</span>
-                <span class="md-list-item-text">{{doc.start}}</span>
-              </div>
-              <div v-else>
-                <span class="md-list-item-text item-title">End Time</span>
-                <span class="md-list-item-text">{{doc.end}}</span>
-              </div>
-              <div class="item-status">
-                <span class="md-list-item-text">{{doc.status}}</span>
-              </div>
-            </md-list-item>
-          </div>
-        </div>
-      </md-list>
+      <List :documents="documents" />
     </div>
   </div>
 </template>
@@ -42,9 +19,11 @@
 <script>
 import { getProjects } from "./../../services/firestore.service";
 import { changeShowDialog } from "./../../services/project-dialog.service";
+import List from "./../list/List";
 
 export default {
   name: "home",
+  components: { List },
   data: function() {
     return {
       size: 520,
@@ -56,39 +35,13 @@ export default {
       changeShowDialog();
     }
   },
-  created: function(){
-      this.$bind('documents', getProjects());
+  created: function() {
+    this.$bind("documents", getProjects());
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.item-title {
-  font-size: 12px;
-}
-
-.project-name{
-  width: 30%;
-}
-
-.color {
-  background: linear-gradient(to bottom, #2980b9, #6dd5fa, #a5def3);;
-  width: 1%;
-  height: inherit;
-  float: left;
-}
-
-.list {
-  float: right;
-  width: 99%;
-}
-
-.item-status {
-  background: linear-gradient(to right, #a8ff78, #78ffd6);
-  padding: 15px;
-  border-radius: 70%;
-}
-
 .home {
   height: inherit;
 }
@@ -111,11 +64,6 @@ export default {
   margin-bottom: auto;
   margin-top: auto;
   max-width: 1000px !important;
-}
-
-.md-list {
-  margin: 10%;
-  margin-top: 5%;
 }
 
 p {
--- src/components/my-projects/MyProjects.vue
+++ src/components/my-projects/MyProjects.vue
@@ -6,79 +6,32 @@
     <div v-else>
       <h1 class="md-display-3">List of Projects</h1>
       <p class="md-subheading">The list contains all your projects.</p>
-      <md-list class="md-double-line">
-        <div v-bind:style="{height: (72 * documents.length) + 'px'}">
-          <div class="color"></div>
-          <div class="list">
-            <md-list-item v-for="doc of documents" :key="doc.id" :to="'/details/' + doc.id">
-              <div class="project-name">
-                <span class="md-list-item-text class item-title">Project Name</span>
-                <span class="md-list-item-text">{{doc.name}}</span>
-              </div>
-              <div v-if="doc.status === 'Active'">
-                <span class="md-list-item-text item-title">Start Time</span>
-                <span class="md-list-item-text">{{doc.start}}</span>
-              </div>
-              <div v-else>
-                <span class="md-list-item-text item-title">End Time</span>
-                <span class="md-list-item-text">{{doc.end}}</span>
-              </div>
-              <div class="item-status">
-                <span class="md-list-item-text">{{doc.status}}</span>
-              </div>
-            </md-list-item>
-          </div>
-        </div>
-      </md-list>
+      <List :documents="documents" />
     </div>
   </div>
 </template>
 
 <script>
 import { getMyProjects } from "./../../services/firestore.service";
+import List from "./../list/List";
 
 export default {
   name: "MyProjects",
+  components: { List },
   data: function() {
     return {
       size: 520,
       documents: []
     };
   },
-  created: function(){
-      this.$bind('documents', getMyProjects());
+  created: function() {
+    this.$bind("documents", getMyProjects());
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.item-title {
-  font-size: 12px;
-}
-
-.project-name {
-  width: 30%;
-}
-
-.color {
-  background: linear-gradient(to bottom, #2980b9, #6dd5fa, #a5def3);
-  width: 1%;
-  height: inherit;
-  float: left;
-}
-
-.list {
-  float: right;
-  width: 99%;
-}
-
-.item-status {
-  background: linear-gradient(to right, #a8ff78, #78ffd6);
-  padding: 15px;
-  border-radius: 70%;
-}
-
-.home {
+.my-projects {
   height: inherit;
 }
 
@@ -89,22 +42,6 @@ export default {
 
 .md-subheading {
   text-align: center;
-}
-
-.empty {
-  display: flex;
-  height: inherit;
-}
-
-.md-empty-state {
-  margin-bottom: auto;
-  margin-top: auto;
-  max-width: 1000px !important;
-}
-
-.md-list {
-  margin: 10%;
-  margin-top: 5%;
 }
 
 p {
