dfb25b8a635a5f274662504ff72e1cc3934b9cb4
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-07 05:16:02 +0200

    Init

---
+++ src/components/my-projects/MyProjects.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="my-projects">
+    <div v-if="documents.length === 0" class="empty">
+      <h1>You don't have projects!</h1>
+    </div>
+    <div v-else>
+      <h1 class="md-display-3">List of Projects</h1>
+      <p class="md-subheading">The list contains all your projects.</p>
+      <md-list class="md-double-line">
+        <div v-bind:style="{height: (72 * documents.length) + 'px'}">
+          <div class="color"></div>
+          <div class="list">
+            <md-list-item v-for="doc of documents" :key="doc.id" :to="'/details/' + doc.id">
+              <div class="project-name">
+                <span class="md-list-item-text class item-title">Project Name</span>
+                <span class="md-list-item-text">{{doc.name}}</span>
+              </div>
+              <div v-if="doc.status === 'Active'">
+                <span class="md-list-item-text item-title">Start Time</span>
+                <span class="md-list-item-text">{{doc.start}}</span>
+              </div>
+              <div v-else>
+                <span class="md-list-item-text item-title">End Time</span>
+                <span class="md-list-item-text">{{doc.end}}</span>
+              </div>
+              <div class="item-status">
+                <span class="md-list-item-text">{{doc.status}}</span>
+              </div>
+            </md-list-item>
+          </div>
+        </div>
+      </md-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getMyProjects } from "./../../services/firestore.service";
+
+export default {
+  name: "MyProjects",
+  data: function() {
+    return {
+      size: 520,
+      documents: []
+    };
+  },
+  created: function(){
+      this.$bind('documents', getMyProjects());
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.item-title {
+  font-size: 12px;
+}
+
+.project-name {
+  width: 30%;
+}
+
+.color {
+  background: linear-gradient(to bottom, #2980b9, #6dd5fa, #a5def3);
+  width: 1%;
+  height: inherit;
+  float: left;
+}
+
+.list {
+  float: right;
+  width: 99%;
+}
+
+.item-status {
+  background: linear-gradient(to right, #a8ff78, #78ffd6);
+  padding: 15px;
+  border-radius: 70%;
+}
+
+.home {
+  height: inherit;
+}
+
+.md-display-3 {
+  text-align: center;
+  margin-top: 8%;
+}
+
+.md-subheading {
+  text-align: center;
+}
+
+.empty {
+  display: flex;
+  height: inherit;
+}
+
+.md-empty-state {
+  margin-bottom: auto;
+  margin-top: auto;
+  max-width: 1000px !important;
+}
+
+.md-list {
+  margin: 10%;
+  margin-top: 5%;
+}
+
+p {
+  margin-block-start: 0;
+}
+</style>
