da544d0bbefaae07c483eb75ff3118fbc2e9bdac
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-07 05:48:38 +0200

    Init

---
+++ src/components/search/Search.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="search">
+    <List :documents="documents" />
+  </div>
+</template>
+
+<script>
+import { getProjects } from "./../../services/firestore.service";
+import List from "./../list/List";
+
+export default {
+  name: "Search",
+  components: { List },
+  data: function() {
+    return {
+      size: 520,
+      documents: []
+    };
+  },
+  created: function() {
+    this.$bind("documents", getProjects());
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.search {
+  height: inherit;
+}
+
+.md-display-3 {
+  text-align: center;
+  margin-top: 8%;
+}
+
+.md-subheading {
+  text-align: center;
+}
+
+</style>
