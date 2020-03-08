df0995200fa263ce72ab4d9a9b084132960cde96
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-08 11:52:41 +0200

    Added Diary

--- src/components/details/Details.vue
+++ src/components/details/Details.vue
@@ -3,24 +3,30 @@
     <div v-if="data" class="wrapper">
       <div class="title-div">
         <h1 class="md-display-3">{{data.name}}</h1>
-        <h2 class="md-display-2">{{data.description}}</h2>
+        <h2 class="md-subheading">{{data.description}}</h2>
       </div>
-      <div class="info">
-        <h3 class="md-display-1">Steps</h3>
-        <div class="steps-div">
-          <Stepper :steps="data.steps" />
+      <div class="second-wrapper">
+        <div class="info">
+          <h3 class="md-display-1">Steps</h3>
+          <div class="steps-div">
+            <Stepper :steps="data.steps" />
+          </div>
+          <h3 class="md-display-1">Members</h3>
+          <div class="chips-div">
+            <Chips class="chips" :members="data.members" />
+          </div>
+          <h3 class="md-display-1">Date</h3>
+          <div class="date-div">
+            <Date :start="data.start" :end="data.end" />
+          </div>
         </div>
-        <h3 class="md-display-1">Members</h3>
-        <div class="chips-div">
-          <Chips class="chips" :members="data.members" />
+        <div class="diary">
+            <h3 class="md-display-1">Diary</h3>
+            <Diary :projectId="data.id" />
         </div>
-        <h3 class="md-display-1">Date</h3>
-        <div class="date-div">
-          <Date :start="data.start" :end="data.end" />
-        </div>
-        <div class="status-div">
-          <Status :status="data.status" :projectId="data.id" />
-        </div>
+      </div>
+      <div class="status-div">
+        <Status :status="data.status" :projectId="data.id" />
       </div>
     </div>
   </div>
@@ -32,6 +38,7 @@ import Stepper from "./Stepper";
 import Chips from "./Chips";
 import Date from "./Date";
 import Status from "./Status";
+import Diary from "./Diary";
 
 export default {
   name: "Details",
@@ -39,7 +46,8 @@ export default {
     Stepper,
     Chips,
     Date,
-    Status
+    Status,
+    Diary
   },
   data: function() {
     return {
@@ -62,8 +70,19 @@ export default {
   margin-right: auto;
 }
 
-.info{
-    padding: 50px;
+.second-wrapper{
+    display: inline-flex;
+    width: 100%;
+}
+
+.info,
+.diary  {
+  width: 50%;
+  min-width: 400px;
+}
+
+.md-display-1{
+    text-align: center;
 }
 
 .title-div {
---
+++ src/components/details/Diary.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Diary",
+  props: ['projectId'],
+  data: function() {
+    return {
+        
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
