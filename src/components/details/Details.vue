36948e60942ed82e0669c3dcb5b3277ac3c95e62
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-08 10:10:42 +0200

    Init and get data from firestore

---
+++ src/components/details/Details.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{data}}</div>
+</template>
+
+<script>
+import { getProject } from './../../services/firestore.service';
+
+export default {
+  name: "Details",
+  data: function() {
+    return {
+        data: null
+    };
+  },
+  created: function() {
+    this.$bind("data", getProject(this.$route.params.id));
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
