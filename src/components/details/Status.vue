401b820158cf1f19a52442e3ffd0bffb4690950f
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-08 11:38:10 +0200

    Added md-field and update status

--- src/components/details/Status.vue
+++ src/components/details/Status.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="status-div">
-    <div class="status">{{status}}</div>
+    <div class="status" @click="edit = !edit">
+      <div v-if="edit">
+        <md-field>
+          <md-select v-model="newStatus" name="status-select" id="status-select">
+            <md-option value="Active">Active</md-option>
+            <md-option value="Closed">Closed</md-option>
+          </md-select>
+        </md-field>
+      </div>
+      <div v-else>{{status}}</div>
+    </div>
   </div>
 </template>
 
 <script>
+import { updateFieldFromProject } from "./../../services/firestore.service";
+
 export default {
   name: "Status",
-  props: ["status"]
+  props: ["status", "projectId"],
+  data: function() {
+    return {
+      edit: true,
+      newStatus: null
+    };
+  },
+  created: function() {
+    this.newStatus = this.status;
+  },
+  watch: {
+    newStatus: function() {
+      if (this.newStatus !== this.status) {
+        updateFieldFromProject(this.projectId, "status", this.newStatus);
+        this.edit = false;
+      }
+    }
+  }
 };
 </script>
 
@@ -19,9 +48,8 @@ export default {
 .status {
   margin: auto;
   background: linear-gradient(to right, #a8ff78, #78ffd6);
-  padding: 15px;
+  padding: 20px;
   border-radius: 70%;
-  width: 25%;
   text-align: center;
   font-size: 22px;
 }
