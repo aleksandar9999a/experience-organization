48cbc95f383fe914b1d5ffc0069771b768567d5e
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-08 15:51:26 +0200

    Render date

--- src/components/details/DiaryItem.vue
+++ src/components/details/DiaryItem.vue
@@ -8,6 +8,10 @@
       <span>{{user.firstName}} {{user.lastName}}</span>
       <span>{{text}}</span>
     </div>
+
+    <div>
+      <span>{{timestamp}}</span>
+    </div>
   </md-list-item>
 </template>
 
@@ -20,12 +24,20 @@ export default {
   data: function() {
     return {
       user: null,
-      text: null
+      text: null,
+      timestamp: null
     };
   },
-  created: function(){
-      this.$bind('user', getUser(this.item.user));
-      this.text = this.item.text;
+  methods: {
+    generateDate(timestamp) {
+      return `${timestamp.getDate()}/${timestamp.getMonth() +
+        1}/${timestamp.getFullYear()}`;
+    }
+  },
+  created: function() {
+    this.$bind("user", getUser(this.item.user));
+    this.text = this.item.text;
+    this.timestamp = this.generateDate(this.item.timestamp.toDate());
   }
 };
 </script>
