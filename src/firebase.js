2f3c7b0f5b3d86b5c147252f9f6d733ebace342e
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-25 01:56:19 +0300

    Revert "Ignore file"
    This reverts commit 18d75f48a21e78ace1ffd52c854959b603d44f63.

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/storage';
 import "@firebase/auth";
 
 const environment = {
-    apiKey: "AIzaSyDI88uhP_qrlocsOwiCzCOcovQUYRRRr1c",
-    authDomain: "experience-organization.firebaseapp.com",
-    databaseURL: "https://experience-organization.firebaseio.com",
-    projectId: "experience-organization",
-    storageBucket: "experience-organization.appspot.com",
-    messagingSenderId: "256827991862",
-    appId: "1:256827991862:web:ac02f4926621024a6fb597",
-    measurementId: "G-B0SDJT3MXV"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 export const db = firebase.initializeApp(environment);
